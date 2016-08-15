165478c288e34ea5010a6ae75e0edf4c0316ab46
cello-j <cello-j@klab.com>
2016-08-16 08:10:02 +0900

    add foundation

--- server/config/local.py
+++ server/config/local.py
@@ -6,4 +6,3 @@ class LocalConfig(BaseConfig):
     GOOGLE_CALLBACK_URL = 'http://localhost:5000/login/oauth2callback'
     STATIC_FOLDER = BaseConfig.BASE_DIR + '/client/static/'
     CLIENT_SECRET = BaseConfig.BASE_DIR + '/auth/google/client_secret_local.json'
-    SERVER_URL = 'localhost:5000'
--- server/modules/upload.py
+++ server/modules/upload.py
@@ -1,6 +1,7 @@
 # coding: utf-8
-from flask import (Blueprint, render_template, session)
-
+from flask import (Blueprint, render_template, request, flash)
+from openpyxl import load_workbook
+import os
 module = Blueprint('upload', __name__)
 
 
@@ -8,3 +9,44 @@ module = Blueprint('upload', __name__)
 def index():
     return render_template("upload.html")
 
+
+@module.route("/read", methods=['POST', 'GET'])
+def read_file():
+    """
+
+    """
+    file=None
+    form = request.form
+    if request.files['file'].filename == '':
+        message = 'アップロードファイルがありません。'
+        flash(message, 'warning')
+        print(message)
+        #raise Exception
+
+    else:
+        file = request.files['file']
+
+    # TODO: check file extension
+    """ root, ext = os.path.splitext(file)
+
+    if ext != '.xlsx':
+        message = 'アップロードしたファイルの拡張子がxlsxではないです。'
+        flash(message, 'warning')
+        #raise Exception
+        print(message)"""
+
+    # TODO: path+filename
+    print(file.filename)
+    file.save(file.filename)
+    wb = load_workbook(file)
+    print(wb.get_sheet_names())
+    for ws in wb.get_sheet_names():
+        sheet_ranges = wb[ws]
+        print(sheet_ranges['A5'].value)
+        """if ws == "Set Phrases":
+          for row in wb[ws].rows:
+                for cell in row:
+                    if cell.value != "None" or cell.value is not None:
+                        print(cell.coordinate, cell.value)"""
+
+    return 'saved'
